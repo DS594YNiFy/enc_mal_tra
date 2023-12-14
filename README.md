68934800e1464b849c35d9b09445b3bf19582b19
DS594YNiFy <CYLK_Cylkun@163.com>
2023-12-14 16:22:23 +0800

    pip install

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# enc_mal_tra
+# enc_mal_tra README
+
+```shell
+pip install tensorflow
+pip install torch
+pip install numpy
+pip install matplotlib
+pip install scikit-learn
+pip install pandas
+```
---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import re
+
+
+def load_data():
+    data_csv_df = pd.read_csv(r"data/temp_ztyk4h3v6s-1.csv", header=0)
+    
+    return data_csv_df
+
+
+def main():
+    load_data()
+
+
+if __name__ == "__main__":
+    main()
